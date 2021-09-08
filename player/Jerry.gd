9663970dc685e672ab9151353b41c7f858b7cbf3
SusieHatter <susiivaldez@gmail.com>
2021-09-08 04:04:21 -0500

    Add players poses and flipping with  direction of movement

--- player/Jerry.gd
+++ player/Jerry.gd
@@ -6,11 +6,22 @@ const JUMPFORCE = -1100
 
 var velocity = Vector2(0,0)
 
+onready var sprite = $Sprite
+
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
-	if Input.is_action_pressed("left"):
+		sprite.play("walk")
+		sprite.flip_h = false
+	elif Input.is_action_pressed("left"):
 		velocity.x = -SPEED
+		sprite.play("walk")
+		sprite.flip_h = true
+	else: 
+		sprite.play("idle")
+	
+	if not is_on_floor():
+		sprite.play("air")
 	
 	velocity.y = velocity.y + GRAVITY
 	

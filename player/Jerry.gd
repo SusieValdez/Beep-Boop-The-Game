37aa66429cb52072d4ab8591fd854878866cc6c5
SusieHatter <susiivaldez@gmail.com>
2021-09-08 03:45:39 -0500

    Add jump and gravity

--- player/Jerry.gd
+++ player/Jerry.gd
@@ -1,12 +1,21 @@
 extends KinematicBody2D
 
+const SPEED = 180
+const GRAVITY = 35
+const JUMPFORCE = -1100
+
 var velocity = Vector2(0,0)
 
 func _physics_process(delta):
 	if Input.is_action_pressed("right"):
-		velocity.x = 100
+		velocity.x = SPEED
 	if Input.is_action_pressed("left"):
-		velocity.x = -100
+		velocity.x = -SPEED
 	
-	move_and_slide(velocity)
+	velocity.y = velocity.y + GRAVITY
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMPFORCE
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x, 0, 0.2)

270b3a5090aa78c83680aaceb2d9e366b42d80b9
SusieHatter <susiivaldez@gmail.com>
2021-09-10 07:53:32 -0500

    Add coins, collision layers and masks

--- player/Jerry.gd
+++ player/Jerry.gd
@@ -8,7 +8,7 @@ var velocity = Vector2(0,0)
 
 onready var sprite = $Sprite
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	if Input.is_action_pressed("right"):
 		velocity.x = SPEED
 		sprite.play("walk")
@@ -30,3 +30,7 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	velocity.x = lerp(velocity.x, 0, 0.2)
+
+
+func _on_Fallzone_body_entered(body):
+	get_tree().change_scene("res://level1.tscn")

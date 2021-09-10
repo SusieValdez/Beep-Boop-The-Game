4ffd7d97e6ccd0bcb9365c55c10f3a3520eb5c81
SusieHatter <susiivaldez@gmail.com>
2021-09-10 08:19:58 -0500

    Add coin count logic and coin bounce animation

--- coin/Coin.gd
+++ coin/Coin.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
-
+onready var animationPlayer = $AnimationPlayer
 
 func _on_Coin_body_entered(body):
+	animationPlayer.play("bounce")
+	body.add_coin()
+
+
+func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
